306544931c299e20e6b73c1beae0475ea4735f26
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2022-12-06 12:08:23 +0100

    Début de création du pop-up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import LoginPopUp from "./components/loginPopUp/LoginPopUp";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<LoginPopUp />} />
       </Routes>
     </div>
   );
