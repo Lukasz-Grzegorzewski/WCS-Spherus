4128b5bcaaf7d2fa4f3d147b401eef5c1165aeff
Edouard de Champeaux <bobdechampeaux@gmail.com>
2022-12-06 11:35:28 +0100

    :sparkles: finalisation du footer + creation des routes et des pages légales

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Footer from "@components/footer/Footer";
+import Policy from "@components/footer/legal_pages/policy/Policy";
+import TermsOfServices from "@components/footer/legal_pages/termsofservices/TermsOfServices";
+import Cookies from "@components/footer/legal_pages/cookies/Cookies";
 import Home from "./pages/Home";
 
 function App() {
@@ -7,6 +10,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/cookies" element={<Cookies />} />
+        <Route path="/termsofservices" element={<TermsOfServices />} />
       </Routes>
       <Footer />
     </div>
