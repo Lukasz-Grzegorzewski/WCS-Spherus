fc1a0589bbacc98cef63c07a48b70f775d269f33
Edouard de Champeaux <bobdechampeaux@gmail.com>
2022-12-08 17:27:05 +0100

    :lipstick: ccs improvment of the section display on the Home page

--- frontend/src/components/section/Section.jsx
+++ frontend/src/components/section/Section.jsx
@@ -2,8 +2,35 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 import VideoCard from "@components/category_video/VideoCard";
+import C from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop2: {
+    breakpoint: { max: 4000, min: 1200 },
+    items: 4,
+    slidesToSlide: 2,
+  },
+  desktop1: {
+    breakpoint: { max: 1200, min: 800 },
+    items: 3,
+    slidesToSlide: 2,
+  },
+  tablette: {
+    breakpoint: { max: 700, min: 550 },
+    items: 2,
+    slidesToSlide: 1,
+  },
+  mobile: {
+    breakpoint: { max: 375, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+};
 
 function Section({ id }) {
+  const Carousel = C.default ? C.default : C;
+
   const [video, setVideo] = useState([]);
 
   const getVideo = () => {
@@ -24,17 +51,23 @@ function Section({ id }) {
 
   return (
     <div className="section_container">
-      {video.map((v) => (
-        <div className="section_card" key={v.id}>
-          <VideoCard
-            id={v.id}
-            url={v.url}
-            title={v.title}
-            description={v.description}
-            display={v.display}
-          />
-        </div>
-      ))}
+      <Carousel
+        containerClass="section_carousel"
+        responsive={responsive}
+        infinite
+      >
+        {video.map((v) => (
+          <div className="section_card" key={v.id}>
+            <VideoCard
+              id={v.id}
+              url={v.url}
+              title={v.title}
+              description={v.description}
+              display={v.display}
+            />
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 }
