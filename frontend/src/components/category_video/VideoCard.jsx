fe1d74eccb16e8f694ff87e3920955a4c5ce6146
Lukasz Grzegorzewski <lukasz@macbook-pro-lukasz.home>
2022-12-09 02:31:44 +0100

    Lukasz-fix bug. VideoCard and backend function: getVideosByCategoryId

--- frontend/src/components/category_video/VideoCard.jsx
+++ frontend/src/components/category_video/VideoCard.jsx
@@ -4,7 +4,9 @@ import HoverVideoPlayer from "react-hover-video-player";
 import { FaLock } from "react-icons/fa";
 
 function VideoCard({ url, title, description, display }) {
-  const videoUrl = `http://localhost:5000/${url}`;
+  const videoUrl = `http://localhost:${
+    import.meta.env.VITE_PORT_BACKEND
+  }${url}`;
 
   return display ? (
     <div className="videocard">
