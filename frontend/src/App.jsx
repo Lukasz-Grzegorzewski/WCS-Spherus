65fe5069bb93ea879036e342bf058ddaabc19c75
Jonathan Scattolini <jscattolini@gmail.com>
2022-12-09 01:00:34 +0100

    :sparkles: Add adminPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import CategoryVideo from "@components/category_video/CategoryVideo";
+import Admin from "@pages/Admin";
 import Footer from "./components/footer/Footer";
 import Policy from "./components/footer/legal_pages/policy/Policy";
 import TermsOfServices from "./components/footer/legal_pages/termsofservices/TermsOfServices";
@@ -17,6 +18,7 @@ function App() {
         <Route path="/termsofservices" element={<TermsOfServices />} />
         <Route path="/categories/:id" element={<CategoryVideo />} />
         <Route path="/registration" element={<Registration />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
       <Footer />
     </div>
