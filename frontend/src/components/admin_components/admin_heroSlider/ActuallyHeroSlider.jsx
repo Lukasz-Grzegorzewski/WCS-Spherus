a25032cfbd3ebdb793034f5149934a9f155ddca8
Jonathan Scattolini <jscattolini@gmail.com>
2022-12-12 11:45:28 +0100

    :hammer: Update component refresh ok

--- frontend/src/components/admin_components/admin_heroSlider/ActuallyHeroSlider.jsx
+++ frontend/src/components/admin_components/admin_heroSlider/ActuallyHeroSlider.jsx
@@ -1,37 +1,56 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import HoverVideoPlayer from "react-hover-video-player";
+import { FaAngleLeft } from "react-icons/fa";
 import ChoiceHero from "./ChoiceHero";
 
-function ActuallyHeroSlider({ title, url, cat }) {
+function ActuallyHeroSlider({ id, title, url, cat, heroInfo, setHeroInfo }) {
   const videoUrl = `http://localhost:${
     import.meta.env.VITE_PORT_BACKEND
   }/${url}`;
 
   const [choice, setChoice] = useState(false);
 
+  const classButton = () => {
+    if (choice === true) {
+      return "actuallyHeroSlider_btn_activ";
+    }
+    return "actuallyHeroSlider_btn_inactiv";
+  };
+
   return (
-    <div className="ActuallyHeroSlider">
-      <div className="ActuallyHeroSlider_video">
+    <div className="actuallyHeroSlider">
+      <p className="actuallyHeroSlider_number">Video {id}</p>
+      <div>
         <HoverVideoPlayer
-          videoClassName="ActuallyHeroSlider_video"
-          className="ActuallyHeroSlider_video"
+          videoClassName="actuallyHeroSlider_video"
           videoSrc={videoUrl}
           muted
         />
       </div>
-      <p>Category : {cat}</p>
-      <p>Title : {title}</p>
-      <button
-        type="button"
-        onClick={() => {
-          setChoice(!choice);
-        }}
-      >
-        Modify
-      </button>
-      <div className="admin_menu_comp_users">
-        {choice === true && <ChoiceHero />}
+      <p className="actuallyHeroSlider_cat">Category : {cat}</p>
+      <p className="actuallyHeroSlider_title">Title : {title}</p>
+      <div className="actuallyHeroSlider_btn">
+        <button
+          className={classButton()}
+          type="button"
+          onClick={() => {
+            setChoice(!choice);
+          }}
+        >
+          Modify <FaAngleLeft className="svg" />
+        </button>
+      </div>
+      <div>
+        {choice === true && (
+          <ChoiceHero
+            id={id}
+            heroInfo={heroInfo}
+            setHeroInfo={setHeroInfo}
+            // setRefresh={setRefresh}
+            // refresh={refresh}
+          />
+        )}
       </div>
     </div>
   );
@@ -42,5 +61,18 @@ export default ActuallyHeroSlider;
 ActuallyHeroSlider.propTypes = {
   title: PropTypes.string.isRequired,
   cat: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
   url: PropTypes.string.isRequired,
+  heroInfo: PropTypes.arrayOf(
+    PropTypes.shape({
+      cat: PropTypes.string.isRequired,
+      date: PropTypes.string.isRequired,
+      display: PropTypes.number.isRequired,
+      hsid: PropTypes.number.isRequired,
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  setHeroInfo: PropTypes.func.isRequired,
 };
