f2d52dc86f6ddbdde5273acf511f8466e163d466
Daniello109 <legolas_lorien@hotmail.com>
2022-12-13 12:18:44 +0100

    dani LD-39

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Navbar from "@components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Policy from "./components/footer/legal_pages/policy/Policy";
 import TermsOfServices from "./components/footer/legal_pages/termsofservices/TermsOfServices";
@@ -9,6 +10,8 @@ import Registration from "./pages/Registration";
 function App() {
   return (
     <div className="App">
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/policy" element={<Policy />} />
