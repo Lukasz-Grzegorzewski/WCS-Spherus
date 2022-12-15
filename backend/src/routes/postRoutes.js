19b24b2cec322501de2c09bfc3ff91e016b134d2
Edouard de Champeaux <bobdechampeaux@gmail.com>
2022-12-15 15:22:43 +0100

    ✨ creation of the admin_video component + update of postRouter and postroutes in backend + install of multer in backend

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,13 +1,26 @@
 const express = require("express");
 
 const router = express.Router();
+const multer = require("multer");
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/assets/videos");
+  },
+  filename: (req, file, cb) => {
+    const date = new Date().getTime();
+    req.body.filename = `${req.body.title + date.toString()}.mp4`;
+    cb(null, req.body.filename.toString());
+  },
+});
+
+const upload = multer({ storage });
 const postRoutesFunctions = require("../handlers/postRoutesFunctions");
 
 router.post("/users", postRoutesFunctions.signInUserByUser);
 router.post("/users/admin", postRoutesFunctions.signInUserByAdmin);
 
-router.post("/videos", postRoutesFunctions.postVideo);
+router.post("/videos", upload.single("file"), postRoutesFunctions.postVideo);
 router.post("/category/video", postRoutesFunctions.attachCategoryToVideo);
 
 router.post("/categories", postRoutesFunctions.postCategory);
--- frontend/src/components/admin_components/admin_videos/AdminVideos.jsx
+++ frontend/src/components/admin_components/admin_videos/AdminVideos.jsx
@@ -1,9 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function AdminVideos() {
+  const [file, setFile] = useState({});
+  const [videoDetails, setVideoDetails] = useState({
+    title: "",
+    description: "",
+    display: "",
+    date: null,
+  });
+
+  const uploadVideo = (data) => {
+    axios
+      .post(
+        `http://localhost:${import.meta.env.VITE_PORT_BACKEND}/videos`,
+        data
+      )
+      .catch(() => {
+        console.error("video not uploaded");
+      });
+  };
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    data.append("title", videoDetails.title);
+    data.append("description", videoDetails.description);
+    data.append("display", videoDetails.display);
+    data.append("date", videoDetails.date);
+    data.append("file", file);
+    uploadVideo(data);
+  };
+
   return (
     <div>
-      <p>YOUHOUHOUOUOU WE ADMIN THE VIDEOS</p>
+      <div className="adminvideo_container">
+        <div>
+          <form action="" onSubmit={handleUpload} className="uplod_video">
+            <label htmlFor="file">Upload a video</label>
+            <input
+              type="file"
+              id="file"
+              name="file"
+              accept=".mp4"
+              onChange={(e) => {
+                setFile(e.target.files[0]);
+              }}
+            />
+
+            <input
+              type="text"
+              id="title"
+              value={videoDetails.title}
+              placeholder="Enter the video title"
+              onChange={(e) =>
+                setVideoDetails({ ...videoDetails, title: e.target.value })
+              }
+            />
+            {videoDetails.title}
+
+            <input
+              type="text"
+              id="description"
+              value={videoDetails.description}
+              placeholder="Enter the video description"
+              onChange={(e) =>
+                setVideoDetails({
+                  ...videoDetails,
+                  description: e.target.value,
+                })
+              }
+            />
+            {videoDetails.description}
+
+            <label htmlFor="display">
+              Display video?{" "}
+              <select
+                className="display"
+                id="display"
+                value={videoDetails.display}
+                onChange={(e) =>
+                  setVideoDetails({ ...videoDetails, display: e.target.value })
+                }
+              >
+                <option value="0">locked</option>
+                <option value="1">available</option>;
+              </select>
+            </label>
+
+            <input
+              type="date"
+              value={videoDetails.date}
+              id="date"
+              placeholder="date of upload"
+              onChange={(e) =>
+                setVideoDetails({ ...videoDetails, date: e.target.value })
+              }
+            />
+            {videoDetails.date}
+            <input type="submit" value="Upload" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
