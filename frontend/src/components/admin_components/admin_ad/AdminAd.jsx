90e7f5b3b5b98c7381884d28c719d51f4bc35d7c
Jonathan Scattolini <jscattolini@gmail.com>
2022-12-19 12:11:35 +0100

    :sparkles: Add new component AdminAdvert

--- frontend/src/components/admin_components/admin_ad/AdminAd.jsx
+++ frontend/src/components/admin_components/admin_ad/AdminAd.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import AddAdvert from "./AddAdvert";
 
 function AdminAd() {
   return (
-    <div>
-      <p>YOUHOUHOUOUOU</p>
+    <div className="adminad">
+      <div className="adminad_add">
+        <h1 className="adminad_add_title">Add new Advertising</h1>
+        <AddAdvert />
+      </div>
     </div>
   );
 }
