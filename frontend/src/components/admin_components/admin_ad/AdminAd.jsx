00242d2cafef6780d384ba0cb210663832dae6f5
Jonathan Scattolini <jscattolini@gmail.com>
2022-12-20 17:17:49 +0100

    :hammer: Update component AdminAdvert

--- frontend/src/components/admin_components/admin_ad/AdminAd.jsx
+++ frontend/src/components/admin_components/admin_ad/AdminAd.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import AddAdvert from "./AddAdvert";
 import UpdateAdvert from "./UpdateAdvert";
 import DeleteAdvert from "./DeleteAdvert";
 
 function AdminAd() {
+  const [pub, setPub] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+
+  const getPub = () => {
+    axios
+      .get(`http://localhost:${import.meta.env.VITE_PORT_BACKEND}/publicities`)
+      .then((res) => {
+        setPub(res.data);
+      });
+  };
+
+  useEffect(() => {
+    getPub();
+  }, [refresh]);
+
   return (
     <div className="adminad">
       <div className="adminad_box">
         <h1 className="adminad_box_title">Add new Advertising</h1>
-        <AddAdvert />
+        <AddAdvert refresh={refresh} setRefresh={setRefresh} />
       </div>
+      <div className="adminad_separate" />
       <div className="adminad_box">
         <h1 className="adminad_box_title">Update Advertising</h1>
         <UpdateAdvert />
       </div>
+      <div className="adminad_separate" />
       <div className="adminad_box">
         <h1 className="adminad_box_title">Delete Advertising</h1>
-        <DeleteAdvert />
+        {pub.length >= 1 && (
+          <DeleteAdvert pub={pub} refresh={refresh} setRefresh={setRefresh} />
+        )}
       </div>
     </div>
   );
