bc100a1d7a4c64b7bd13116df1e1bc5f64cc5d70
Jonathan Scattolini <jscattolini@gmail.com>
2023-01-09 10:53:35 +0100

    :lipstick: Update CSS on AdminHome & Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,22 +6,9 @@ export default function Home() {
   return (
     <div className="home">
       <Heroslider />
-      <HomeDisplay />
-      {/* <div className="home_section">
-        {section.map((e) => (
-          <div key={e.id}>
-            <div className="home_section_container">
-              <NavLink to={`/categories/${e.id}`}>
-                <div>{e.name}</div>
-              </NavLink>
-              <NavLink to={`/categories/${e.id}`}>
-                <div className="home_section_container_seebtn">See More</div>
-              </NavLink>
-            </div>
-            <Section id={e.id} />
-          </div>
-        ))}
-      </div> */}
+      <div className="home_display">
+        <HomeDisplay />
+      </div>
     </div>
   );
 }
